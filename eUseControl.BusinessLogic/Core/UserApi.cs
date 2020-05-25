af23498680562facbea058e1db92a6a34af0914d
”VictoriaRudaca” <victoria.rudaca@isa.utm.md>
2020-05-25 19:14:24 +0300

    web

---
+++ eUseControl.BusinessLogic/Core/UserApi.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using eUseControl.BusinessLogic.DBModel;
+using eUseControl.Domain.Entities.User;
+
+namespace eUseControl.BusinessLogic.Core
+{
+    public class UserApi
+    {
+        internal ULoginResp UserLoginAction(ULoginData data)
+        {
+            UDbTable user;
+
+            using (var db = new UserContext())
+            {
+                user = db.Users.FirstOrDefault(u => u.Username == data.Credential);
+            }
+
+            using (var db = new UserContext())
+            {
+                user = (from u in db.Users where u.Username == data.Credential select u).FirstOrDefault();
+            }
+
+
+            if (user != null)
+            {
+
+            }
+
+            return new ULoginResp();
+        }
+    }
+}
